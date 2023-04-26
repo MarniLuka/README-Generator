69b55089d9e2301692a6b3dacb72823344b54d05
Marni Luka <marnixluka@gmail.com>
2023-04-26 18:08:59 -0400

    moved files, downloaded inquirer, and added them to the js file

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Generates the questions
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
