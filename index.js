217faff89550d4717f6d25f69221115ac74f8960
Marni Luka <marnixluka@gmail.com>
2023-04-26 18:37:32 -0400

    added prompt questions

--- index.js
+++ index.js
@@ -3,6 +3,70 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // Generates the questions
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is the title of your project?',
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: 'Write the description of your project:',
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: 'Explain (if any) installation instructions:',
+            name: 'installation',
+        },
+        {
+            type: 'input',
+            message: 'Include usage information:',
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: 'List collaborators (if any), third-party assets (if any), and/or tutorials followed (if any):',
+            name: 'credits',
+        },
+        {
+            type: 'list',
+            message: 'Select a license:',
+            name: 'license',
+            choices: [
+                'None',
+                'Apache License 2.0',
+                'GNU General Public License v3.0',
+                'MIT License',
+                'BSD 2-Clause "Simplified" License',
+                'BSD 3-Clause "New" or "Revised" License',
+                'Boost Software License 1.0',
+                'Creative Commons Zero v1.0 Universal',
+                'Eclipse Public License 2.0',
+                'GNU Affero General Public License v3.0',
+                'GNU General Public License v2.0',
+                'GNU Lesser General Plubic LIcense v2.1',
+                'Mozilla Public License 2.0',
+                'The Unlicense',
+            ],
+        },
+        {
+            type: 'input',
+            message: 'Enter test instructions:',
+            name: 'test',
+        },
+        {
+            type: 'input',
+            message: 'What is your GitHub username?',
+            name: 'username',
+        },
+        {
+            type: 'input',
+            message: 'What is your email address?',
+            name: 'email',
+        },
+    ])
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
